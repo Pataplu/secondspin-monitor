8489b712e0daf075fcd8a4d52c5f560bb5501469
Pataplu <79833770+Pataplu@users.noreply.github.com>
2026-01-26 15:23:49 +0100

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,1 +1,13 @@
-print("SecondSpin agent draait")
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://www.secondspin.nl/shop/nieuw-binnen"
+
+def run():
+    response = requests.get(URL)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    albums = soup.select(".product-item")
+    print(f"Aantal albums gevonden: {len(albums)}")
+
+run()
